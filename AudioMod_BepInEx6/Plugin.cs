865721501f5eb6c795699eb8e5beb58466393e3f
Patrick <pvanhalm00@gmail.com>
2025-10-31 12:38:39 +0100

    Update plugin version to 0.1.2

--- AudioMod_BepInEx6/Plugin.cs
+++ AudioMod_BepInEx6/Plugin.cs
@@ -8,7 +8,7 @@ using Utils.Logging;
 
 namespace AudioMod
 {
-    [BepInPlugin("nl.pvanhalm.plugins.greyhack.audio-mod", "Audio Mod", "0.1.1")]
+    [BepInPlugin("nl.pvanhalm.plugins.greyhack.audio-mod", "Audio Mod", "0.1.2")]
     public class Plugin : BaseUnityPlugin
     {
         private bool _initialized;
